946e11de13a6f4f6c6493b5f8ac7550ff325ff8b
Leonardo Otaviano Pedrozo <lpedrozo@tzm.com.br>
2021-11-09 11:18:12 -0300

    Initial import.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,20 @@ import { PageLogin } from "./pages/Login";
 import { PageMain } from "./pages/Main";
 import { UserProvider } from "./utilidades/Auth";
 import { buscar } from "./utilidades/Fetch";
+import {addLocale, locale} from "primereact/api";
+
+addLocale("pt-BR", {
+	firstDayOfWeek: 1,
+	dayNames: ["domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado"],
+	dayNamesShort: ["dom", "seg", "ter", "qua", "qui", "sex", "sáb"],
+	dayNamesMin: ["D", "S", "T", "Q", "Q", "S", "S"],
+	monthNames: ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"],
+	monthNamesShort: ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"],
+	today: "Hoje",
+	clear: "Limpar"
+});
+
+locale("pt-BR");
 
 export default function App() {
 	const [usuario, setUsuario] = useState();
