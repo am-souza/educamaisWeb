a2d1c3dea6ee0cd16f10b0b30fa12321a99e4c6a
Leonardo Otaviano Pedrozo <lpedrozo@tzm.com.br>
2021-11-09 11:54:42 -0300

    Initial import.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
-import { Panel } from "primereact/panel";
-import { handleLogin } from "../utilidades/Auth";
-import { Button } from "primereact/button";
-import { InputText } from "../components/InputText";
-import { PanelContent } from "../components/PanelContent";
+import React, {useState} from "react";
+import {Panel} from "primereact/panel";
+import {handleLogin} from "../utilidades/Auth";
+import {Button} from "primereact/button";
+import {InputText} from "../components/InputText";
+import {PanelContent} from "../components/PanelContent";
+import {PanelFooter} from "../components/PanelFooter";
+
+import "./Pages.scss";
 
 export function PageLogin(props) {
 	const [login, setLogin] = useState({
@@ -11,13 +14,16 @@ export function PageLogin(props) {
 		password: ""
 	});
 	return (
-				
-		<Panel header="Autenticação">
-			<PanelContent>
-				<InputText label="Login" width={12} value={login.username} onChange={e => setLogin({...login, username: e.target.value})}/>
-				<InputText type="password" label="Senha" width={12} value={login.password} onChange={e => setLogin({...login, password: e.target.value})}/>
-				<Button onClick={() => handleLogin(login, props.onLoginSuccessful)}>Login</Button>
-			</PanelContent>
-		</Panel>
+		<div className="login-form">
+			<Panel header="Autenticação">
+				<PanelContent>
+					<InputText label="Login" width={12} value={login.username} onChange={e => setLogin({...login, username: e.target.value})}/>
+					<InputText type="password" label="Senha" width={12} value={login.password} onChange={e => setLogin({...login, password: e.target.value})}/>
+				</PanelContent>
+				<PanelFooter>
+					<Button icon="pi pi-sign-in" label="Login" onClick={() => handleLogin(login, props.onLoginSuccessful)} />
+				</PanelFooter>
+			</Panel>
+		</div>
 	);
 }
