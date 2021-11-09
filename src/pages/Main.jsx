fe60b8162a450da8149e02dc6268cb12ec11efa6
Leonardo Otaviano Pedrozo <lpedrozo@tzm.com.br>
2021-11-09 17:52:33 -0300

    Initial import.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,26 +1,22 @@
-import React, { useEffect } from "react";
-import { Menubar } from "primereact/menubar";
-import { Button } from "primereact/button";
-import { handleLogout, handleMenu, withUser } from "../utilidades/Auth";
-import { Route, Switch, withRouter } from "react-router-dom";
-import { PageHome } from "./Home";
-import { EditUsuario, PageUsuario } from "./Usuario";
-import { EditQuestao, PageQuestao } from "./Questao";
+import React, {useState} from "react";
+import {Menubar} from "primereact/menubar";
+import {Button} from "primereact/button";
+import {handleLogout, handleMenu, withUser} from "../utilidades/Auth";
+import {Route, Switch, withRouter} from "react-router-dom";
+import {PageHome} from "./Home";
+import {EditUsuario, PageUsuario} from "./Usuario";
+import {EditQuestao, PageQuestao} from "./Questao";
 import {EditAtividade, PageAtividade} from "./Atividade";
-import { PageAvaliacao } from "./Avaliacao";
-import { EditTurma, PageTurma } from "./Turma";
-import { Spacer } from "../components/Spacer";
-import { EditRespostas, PageRespostas } from "./Respostas";
-import { EditMateria, PageMateria } from "./Materia";
-import { EditCurso, PageCurso } from "./Curso";
-import { Avatar } from 'primereact/avatar';
-import { useState} from "react";
+import {PageAvaliacao} from "./Avaliacao";
+import {EditTurma, PageTurma} from "./Turma";
+import {Spacer} from "../components/Spacer";
+import {EditRespostas, PageRespostas} from "./Respostas";
+import {EditMateria, PageMateria} from "./Materia";
+import {EditCurso, PageCurso} from "./Curso";
 import {buscar, json} from "../utilidades/Fetch";
-import { Tag } from 'primereact/tag';
-
+import {Tag} from "primereact/tag";
 
 export const PageMain = withUser(withRouter(((props) => {
-	
 	const [usuarios, setUsuarios] = useState([]);
 /*
 	useEffect(()=>{
@@ -29,24 +25,19 @@ export const PageMain = withUser(withRouter(((props) => {
 */
 	function handleList() {
 		buscar("/usuarios/eu").then(json).then(setUsuarios);
-	}	
-
+	}
 	return (
 		<div>
-
 			<div className="p-grid">
 				<div className="p-col-9" >
-					<Menubar model={handleMenu(props)} end={<Button icon="pi pi-fw pi-power-off" label="Logout" onClick={handleList}/>}/>
+					<Menubar model={handleMenu(props)} end={<Button icon="pi pi-fw pi-power-off" label="Logout" onClick={handleLogout}/>}/>
 				</div>
 				<div className="p-col-3  p-mt-3">					
-					<Tag className="p-mr-1 " label="XP" icon="pi pi-flag" value={usuarios.xp} style={{ backgroundColor: '#2196F3', color: '#ffffff' }}>XP</Tag>	
-					<Tag className="p-mr-1" label="Cash" icon="pi pi-dollar" value={usuarios.cash} style={{ backgroundColor: '#2196F3', color: '#ffffff' }}> CASH</Tag>					
-					<Tag className="p-mr-2" icon="pi pi-user" value={usuarios.username} style={{ backgroundColor: '#2196F3', color: '#ffffff' }}></Tag>	
+					<Tag className="p-mr-1 " label="XP" icon="pi pi-flag" value={usuarios.xp} style={{backgroundColor: "#2196F3", color: "#ffffff"}}>XP</Tag>
+					<Tag className="p-mr-1" label="Cash" icon="pi pi-dollar" value={usuarios.cash} style={{backgroundColor: "#2196F3", color: "#ffffff"}}>CASH</Tag>
+					<Tag className="p-mr-2" icon="pi pi-user" value={usuarios.username} style={{backgroundColor: "#2196F3", color: "#ffffff"}}/>
 				</div>
 			</div>
-			
-			<div>
-
 			<Spacer/>
 			<Switch>
 				<Route exact path="/" component={PageHome}/>
@@ -66,7 +57,6 @@ export const PageMain = withUser(withRouter(((props) => {
 				<Route exact path="/cursos" component={PageCurso}/>
 				<Route exact path="/cursos/:id" component={EditCurso}/>
 			</Switch>
-			</div>	
 		</div>
 	);
 })));
