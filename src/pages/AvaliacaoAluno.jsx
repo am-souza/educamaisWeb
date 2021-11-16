d157007dc26a895bf6391a4645ddc930125cc8ae
Alexandre Souza <alexandremarcosjr@gmail.com>
2021-11-16 13:54:58 -0300

    Tela AvaliacaoAluno

--- src/pages/AvaliacaoAluno.jsx
+++ src/pages/AvaliacaoAluno.jsx
@@ -2,48 +2,58 @@ import {Panel} from "primereact/panel";
 import {DataTable} from "primereact/datatable";
 import React, {useEffect, useState, Row} from "react";
 import {Column} from "primereact/column/column.cjs";
-import {Spacer} from "../components/Spacer";
 import {buscar, excluir, json, salvar} from "../utilidades/Fetch";
 import {InputText} from "../components/InputText";
 import {PanelContent} from "../components/PanelContent";
 import {useParams, withRouter} from "react-router-dom";
 import {Button} from "primereact/button";
 import {PanelFooter} from "../components/PanelFooter";
-import moment from "moment";
-import {AutoComplete} from "../components/AutoComplete";
-import {Calendar} from "../components/Calendar";
 import {Checkbox} from "primereact/checkbox";
 import {byKeyOrId} from "../utilidades/FilterUtils";
+import {AutoComplete} from "../components/AutoComplete";
+
 
 
 export const PageAvaliacaoAluno = withRouter((props) => {
 
 	const  id  = props.match.params.id;
+	const [avaliacao, setAvaliacao] = useState([]);
+	const [atividade, setAtividade] = useState([]);
+	const [questoes, setQuestoes] = useState([]);	 
+	const [escolhas, setEscolhas] = useState([]);  
+	const [questao, setQuestao] = useState([]);
 
-	const [avaliacao, setAvaliacao] = useState([]);  
 
-	useEffect(() => id !== "0" && buscar(`/avaliacoes/${id}`).then(json).then(setAvaliacao), [id]);
+	useEffect(() => buscar(`/avaliacoes/${id}`).then(json).then(setAvaliacao),
+		//buscar(`/atividades/${avaliacao.atividade.id}`).then(json).then(setAtividade),
+		//setAtividade(avaliacao.atividade),
+		//setQuestoes(atividade.questoes),
+		console.log(avaliacao),
+		console.log(id),
+		[id]);
 
-    const atividade = avaliacao.atividade;
-    const questoes = atividade.questoes;
-   
+
+	function handleIniciar(){
+		buscar(`/avaliacoes/${id}`).then(json).then(setAvaliacao);		
+		buscar(`/atividades/${avaliacao.atividade.id}`).then(json).then(setAtividade);	
+		setQuestoes(atividade.questoes);		
+	}	
+	
 	return (
 		<Panel header="Prova">
-			<PanelContent>
-            <DataTable emptyMessage="Nenhum registro encontrado" value={questoes}>
-				<Column className="p-col-1" header="Questão" field="texto"/>
-                <Column header="Resposta" body={r => (
-                            
-							<Row value={r.texto} onChange={e => {
-								questoes.escolhas.filter(q => byKeyOrId(q, r)).forEach(r => r.texto = e.target.value);
-							}}/>
-						)}/>
-						<Column style={{textAlign: "center", width: "6em"}} header="Correta?" body={r => (
-							<Checkbox checked={r.correta}/>
-						)}/>	
-                                		
-			</DataTable>
-			</PanelContent>			
-		</Panel>
+			<Button label="Iniciar" icon="pi pi-fw pi-search" onClick={handleIniciar}/>
+			<br></br>
+			{avaliacao.atividade.questoes.map(q => 	
+				<div>
+					<h3>{q.texto}</h3>	
+					<DataTable emptyMessage="Nenhum registro encontrado" value={q.escolhas}>
+						<Column className="p-col-1" header="Respostas" field="texto"/>
+						<Column className="p-col-1" header="Correta?">
+							<Checkbox></Checkbox>
+						</Column>
+ 					</DataTable>				
+				</div>
+			)}	
+	</Panel>
 	);
 });
