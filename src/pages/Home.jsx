a5d4625ba43ef84f7df12c9f51da6ada18ce1042
Alexandre Souza <alexandremarcosjr@gmail.com>
2021-11-20 00:44:56 -0300

    Completo v1.0

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,6 +15,7 @@ import {Panel} from "primereact/panel";
 export const PageHome = withUser(withRouter((props) => {
 
 	const [avaliacoes, setAvaliacoes] = useState([]);
+	const [avaliacoesconcluidas, setAvalicoesconcluidas] = useState([]);
 	
 	useEffect(() => {
 		buscar(`/turmas?alunos.id==${props.usuario.id}`).then(json).then(turmas => {
@@ -28,16 +29,31 @@ export const PageHome = withUser(withRouter((props) => {
 		});
 	}, [props.usuario.id]);
 
+		buscar(`/avaliacoesalunos?aluno.id==${props.usuario.id}`).then(json).then(setAvalicoesconcluidas);
+
 	return (
 		<div className="p-grid">			
-			<div className="p-col-7">
+			<div className="p-col-7"><div className="p-mb-3 p-text-center p-text-bold">ATIVIDADES PENDENTES</div>
 				<DataTable emptyMessage="Nenhum registro encontrado" value={avaliacoes} onRowDoubleClick={e => props.history.push(`/avaliacaoaluno/${avaliacoes[e.index].id}`)}>
 					<Column header="Atividade" field="atividade.nome"/> 								
 					<Column header="Turma" field="turma.nome"/>
 					<Column header="Início" body={a => moment(a.inicio).format("DD/MM/YYYY HH:mm")}/>
 					<Column header="Fim" body={a => moment(a.fim).format("DD/MM/YYYY HH:mm")}/>
 				</DataTable>
+				
+				<div className="p-mt-6">
+					<div className="p-mb-3 p-text-center p-text-bold">ATIVIDADES CONCLUÍDAS</div>
+					<DataTable emptyMessage="Nenhum registro encontrado" value={avaliacoesconcluidas}>
+						<Column header="Avaliação" field="avaliacao.atividade.nome"/> 								
+						<Column header="Turma" field="avaliacao.turma.nome"/>
+						<Column header="Nota" field="nota"/>
+					</DataTable>
+				</div>
+				
 			</div>		
+
+
+
 			<div className="p-col-3 p-mt-6 p-offset-1">	
 				<img src={avatar} alt="Sem Texto" width="250"/>	
 				<div className="p-text-center">Olá {props.usuario.nome}!<br/> Não se esqueça de fazer suas atividades e resgatar as recompensas</div>						
